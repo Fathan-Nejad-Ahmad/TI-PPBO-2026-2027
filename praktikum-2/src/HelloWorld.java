b668d64b745e1779387c204fa11c85824880519a
Fathan Nejad Ahmad <ahmadfathannejad@gmail.com>
2026-09-13 22:19:25 +0700

    A.2.Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Fathan Nejad Ahmad");
+        System.out.print("Nama: ");
+        System.out.print("Fathan Nejad Ahmad");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }

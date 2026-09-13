6b98817d24a355c7eee82a19da74eb6f98dfa60a
Fathan Nejad Ahmad <ahmadfathannejad@gmail.com>
2026-09-13 22:24:20 +0700

    A.3.Komentar Kode

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -2,8 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Fathan Nejad Ahmad");
         System.out.print("Nama: ");
+        // Program menampilkan biodata sederhana
         System.out.print("Fathan Nejad Ahmad");
         System.out.println();
-        System.out.println("Ini baris baru");
+        /* Baris di bawah ini
+         mencetak NIM mahasiswa */
+        System.out.println("NIM: 202557310102");
+
     }
 }

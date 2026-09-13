e2c8929ec4c1a4fbcbefcc6b7878508a91aeb9df
Fathan Nejad Ahmad <ahmadfathannejad@gmail.com>
2026-09-13 22:42:30 +0700

    B.2.Casting (Konversi Tipe Data)

--- praktikum-2/src/OperatorDemo.java
+++ praktikum-2/src/OperatorDemo.java
@@ -6,5 +6,9 @@ public class OperatorDemo {
         System.out.println("kali: " + (a * b));
         System.out.println("bagi: " + (a / b));
         System.out.println("hasil bagi: " + (a % b));
+        int nilaiujian= 80;
+        boolean hadir =true;
+        System.out.println("lulus (>=75):" +(nilaiujian >= 75));
+        System.out.println("lulus dan hadir: " +(nilaiujian >= 75 && hadir));
     }
 }

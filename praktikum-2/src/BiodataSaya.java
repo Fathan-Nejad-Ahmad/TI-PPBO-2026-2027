75a7122696bdfcb0df9a612b916927802dfae156
Fathan Nejad Ahmad <ahmadfathannejad@gmail.com>
2026-09-13 22:50:23 +0700

    Praktikum 2 – Sintaks Dasar, Variabel & Operator

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,10 @@
+public class BiodataSaya {
+    public static void main(String[] args) {
+        //program menampilkan biodata saya
+        System.out.print("-Nama:Fathan Nejad Ahmad -");
+        //baris ini di gunakan untuk menampilkan nim
+        System.out.print("Nim:2025573010102-");
+        //baris ini di gunakan untuk menampilkan prodi
+        System.out.print("Program Studi:Teknik Informatika");
+    }
+}
---
+++ praktikum-2/src/Variabel.java
@@ -0,0 +1,13 @@
+public class Variabel {
+    public static void main(String[] args) {
+        int jumlahsaudara= 3;
+        double beratbadan=45;
+        char golongandarah='O';
+        boolean punyakendaraan=true;
+
+        System.out.println("jumlah saudara: " + jumlahsaudara);
+        System.out.println("berat badan: " + beratbadan);
+        System.out.println("golongan darah:" + golongandarah);
+        System.out.println("punyakendaraan:" + punyakendaraan);
+    }
+}
